92b994a61c0d7b2d39242aa251ef71ebb0ea7918
mariuszmitrus <52783121+mariuszmitrus@users.noreply.github.com>
2019-07-11 13:25:26 +0200

    Add files via upload

---
+++ js/whCheckCookies.js
@@ -0,0 +1,14 @@
+
+function WHCheckCookies(){
+  if(!localStorage.cookies_accepted) {
+    var cookies_message = document.getElementById("cookies-message"); 
+    cookies_message.style.display="block" 
+  } 
+} 
+window.onload = WHCheckCookies;
+
+function WHCloseCookiesWindow(){ 
+  localStorage.cookies_accepted = true; 
+  document.getElementById("cookies-message-container").removeChild(document.getElementById("cookies-message"));
+}
+
